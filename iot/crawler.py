af603c64027fd492813a24a696be746f38dbe1db
Ondřej Garncarz <ondrej@garncarz.cz>
2016-11-04 17:48:09 +0100

    Flask: phone number as an input.

--- iot/crawler.py
+++ iot/crawler.py
@@ -9,8 +9,8 @@ from . import settings
 URL = 'https://api.pripoj.me/message/get/{dev_eui}?token={token}'
 
 
-def get(dev_eui=getattr(settings, 'DEV_EUI'),
-        token=getattr(settings, 'TOKEN')):
+def get(dev_eui=getattr(settings, 'DEV_EUI', None),
+        token=getattr(settings, 'TOKEN', None)):
     g = Grab()
     resp = g.go(URL.format(dev_eui=dev_eui, token=token))
     return resp.json
--- iot/webapp.py
+++ iot/webapp.py
@@ -1,25 +1,34 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, render_template, redirect, url_for, request,
+)
 
-from . import crawler, parser, gosms
+from . import crawler, parser, gosms, settings
 
 
 app = Flask(__name__)
 
 
+def get_last_record():
+    batch = crawler.get()
+    return parser.parse(batch['records'][0])
+
+
 @app.route('/')
 def index():
-    batch = crawler.get()
-    rec = parser.parse(batch['records'][-1])
-
-    return render_template('index.html', rec=rec)
+    return render_template('index.html', rec=get_last_record())
 
 
-@app.route('/sms')
+@app.route('/sms', methods=['POST'])
 def sms():
-    batch = crawler.get()
-    rec = parser.parse(batch['records'][-1])
+    if not getattr(settings, 'CAN_SEND_SMS', False):
+        return redirect(url_for('index'))
 
+    phone = request.form['phone']
+    if not phone:
+        return redirect(url_for('index'))
+
+    rec = get_last_record()
     msg = render_template('sms.txt', rec=rec)
-    gosms.send(msg)
+    gosms.send(msg, recipients=phone)
 
     return render_template('sms_sent.html')
