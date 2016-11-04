8aa2b5074ab6375c539f9665a458e3d79a0c594f
Ondřej Garncarz <ondrej@garncarz.cz>
2016-11-04 17:12:50 +0100

    Flask: sending SMS.
    Base template extracted.

--- iot/webapp.py
+++ iot/webapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from . import crawler, parser
+from . import crawler, parser, gosms
 
 
 app = Flask(__name__)
@@ -12,3 +12,14 @@ def index():
     rec = parser.parse(batch['records'][-1])
 
     return render_template('index.html', rec=rec)
+
+
+@app.route('/sms')
+def sms():
+    batch = crawler.get()
+    rec = parser.parse(batch['records'][-1])
+
+    msg = render_template('sms.txt', rec=rec)
+    gosms.send(msg)
+
+    return render_template('sms_sent.html')
