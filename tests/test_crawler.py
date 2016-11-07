b9a6c1a638909f8d5f22bad7133dd2df5ea51c75
Ondřej Garncarz <ondrej@garncarz.cz>
2016-11-07 23:12:11 +0100

    Not testing crawler if there's no device to be fetched.

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,6 +1,10 @@
-from iot import crawler
+import pytest
+
+from iot import crawler, settings
 
 
+@pytest.mark.skipif(not hasattr(settings, 'DEV_EUI'),
+                    reason='No device registered.')
 def test_get():
     resp = crawler.get()
     assert '_meta' in resp
